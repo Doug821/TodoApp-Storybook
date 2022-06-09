3cb5d3f8c7cd7b4e26e968cda70922375221bf0e
Doug821 <dougsilva821@gmail.com>
2022-06-09 09:02:58 -0300

    feature: added props context

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const {default: Input} = require('./components/Input/Input');
 const {default: Card} = require('./components/Card/Card');
 const {default: Home} = require('./Scenes/home/home');
 const {default: Second} = require('./Scenes/second/second');
+const {default: PropsProvider} = require('./Contexts/PropsProvider');
 
-export {Button, Input, Card, Home, Second};
+export {Button, Input, Card, Home, Second, PropsProvider};
